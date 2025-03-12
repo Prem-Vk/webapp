0b929368c3db1dc4010170e50188ec5c6b72ad0e
Prem-Vk <prem.vishwakarma@instamojo.com>
2025-03-12 14:30:29 +0530

    Updated view to use ModelViewSet
    Added custom list function to use postgres full text search for relevance search.

--- candidate/views.py
+++ candidate/views.py
@@ -1,60 +1,38 @@
 from candidate.models import Candidate
 from rest_framework.response import Response
-from candidate.serializers import CandidateSerialiazer, CandidateSearchSerializer
-from rest_framework.decorators import api_view
-from django.db.models import Q, Count
+from candidate.serializers import CandidateSerialiazer
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.decorators import action
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
-class CandidateViewSet(viewsets.ViewSet):
+class CandidateViewSet(viewsets.ModelViewSet):
+    #  We are using model view set for create, update and delete functions.
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerialiazer
     http_method_names = ['get', 'post', 'put', 'delete']
 
-    def create(self, request):
-        serializer = CandidateSerialiazer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #  Modifed list method to show relevance response based on search name.
+    def list(self, request):
+        search_by = request.query_params.get('search', None)
+        if search_by:
+            search_keywords = search_by.split()
+            combinations = []
+            combinations.append(' '.join(search_keywords))
+            if len(search_by) > 1:
+                for i in range(len(search_keywords)):
+                    partial_name = ' '.join(search_keywords[:i] + search_keywords[i+1:])
+                    combinations.append(partial_name)
 
-    def update(self, request, pk=None):
-        try:
-            candidate = Candidate.objects.get(pk=pk)
-        except Candidate.DoesNotExist:
-            return Response({'detail': 'Candidate Does not exists!!'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = CandidateSerialiazer(candidate, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        try:
-            candidate = Candidate.objects.get(pk=pk)
-        except Candidate.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        candidate.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False, methods=['get'])
-    def search(self, request):
-        name = request.query_params.get('name', None)
-        search_keywords = name.split()
-
-        filters = Q()
-        for word in search_keywords:
-            filters |= Q(name__icontains=word)
-
-        candidates = Candidate.objects.filter(filters)
-
-        candidates = candidates.annotate(
-            name_match_count = Count(
-                'id',
-                filter=Q(name__icontaines=search_keywords[0])
-            )
-        ).order_by('-name_match_count')
-
-        serializer = CandidateSearchSerializer(candidates, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            #  We are using postgres full text search here for getting search reponse in relevance:
+            #  Ref:- https://pganalyze.com/blog/full-text-search-django-postgres
+            results = Candidate.objects.none()
+            for combo in combinations:
+                search_query = SearchQuery(combo)
+                search_vector = SearchVector('name')
+                partial_results = Candidate.objects.annotate(
+                    rank=SearchRank(search_vector, search_query)
+                ).filter(rank__gt=0).order_by('-rank').values_list('name', flat=True)
+                results |= partial_results
+            return Response({'result':results}, status=status.HTTP_200_OK)
+        return Response({'error': 'Please provide a search name.'}, status=status.HTTP_400_BAD_REQUEST)
